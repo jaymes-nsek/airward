1dd35a2e1143167689f8f54df0b9a958374e25c8
jaymes.nsek <jaymes.nsek@gmail.com>
2026-01-06 13:51:05 +0100

    chore(deps): add MUI core and recommended peer dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+
+const theme = createTheme({
+    // Customise palette/typography later
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+      </ThemeProvider>
   </StrictMode>,
 )
